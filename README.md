d980920b5a358bdb1af18b754e5eddf8e5c38a91
Leo Nardo <sal1hard@protonmail.com>
2021-07-11 13:15:06 +0300

    Add parameters, documentation and some exceptions

---
+++ README.md
@@ -0,0 +1,28 @@
+# CRO.Py
+> Crop a bunch of images within CLI
+
+Simple python utility which uses [Pillow](https://pillow.readthedocs.io) to crop most popular types of images.
+
+![](img.jpg)
+
+## Pulling up dependencies
+```shell
+pip install Pillow
+```
+
+## Usage example
+```shell
+python cro.py --source '/Users/user/Pictures/' --destination '/Users/user/Pictures/cropy/' -left '50' -top '10'
+```
+
+## Authors
+* [Gleb Savelev](https://github.com/nardo-leo)
+
+See also the [list of contributors](https://github.com/nardo-leo/cropy/graphs/contributors) who participated in this project.
+
+## Contributing
+1. Fork it
+2. Create your feature branch `git checkout -b feature/fooBar`
+3. Commit your changes `git commit -am 'Add some fooBar'`
+4. Push to the branch `git push origin feature/fooBar`
+5. Create a new Pull Request
--- cro.py
+++ cro.py
@@ -1,43 +1,109 @@
 import os
+
+from argparse import ArgumentParser
+from pathlib import Path
 from PIL import Image, UnidentifiedImageError
 
 
-# get current working dir
-cwd = os.path.abspath(os.getcwd())
-# TODO and dirname with targets from params
-dirname_to_crop = '/gen_4/'
+# read cli arguments
+parser = ArgumentParser()
 
-# get filenames of  targets
-# TODO join pathes with os.path.join()
-_, _, filenames = next(os.walk(f'{cwd}{dirname_to_crop}'))
+# set dirs
+parser.add_argument('-s', '--source', dest='source',
+                    help='where to get source images')
+parser.add_argument('-d', '--destination', dest='destination',
+                    help='where to place cropped images')
 
+'''
+Use Pillow documentation for more information about its params
+https://pillow.readthedocs.io/en/stable/reference/Image.html?highlight=crop#PIL.Image.Image.crop
+'''
+# set crop params
+parser.add_argument('-l', '--left', dest='left',
+                    help='crop from the left')
+parser.add_argument('-t', '--top', dest='top',
+                    help='crop from the top')
+parser.add_argument('-r', '--right', dest='right',
+                    help='crop from the right')
+parser.add_argument('-b', '--bottom', dest='bottom',
+                    help='crop from the bottom')
+
+args = parser.parse_args()
+
+
+# get filenames
+try:
+    _, _, filenames = next(os.walk(args.source))
+except Exception:
+    print('Pass -s parameter with absolute source directory path!', '\n')
+    raise ValueError
+
+# create destination dir if it not exists
+try:
+    Path(args.destination).mkdir(parents=True, exist_ok=True)
+except TypeError:
+    # and use source dir like destination if -d is empty
+    args.destination = args.source
+
+
+# do crop job and count it
 counter = 0
 # crop all target files
 for filename in filenames:
     # split filename and extension
     name, ext = os.path.splitext(filename)
+    target_path = os.path.join(args.source, filename)
 
-    # TODO join pathes with os.path.join()
-    path_to_target = f'{cwd}{dirname_to_crop}{filename}'
-
-    # open target image
+    # open image
     try:
-        with Image.open(path_to_target) as im:
-            width, height = im.size
+        with Image.open(target_path) as image:
+            width, height = image.size
 
-            # TODO set crop settings in params
-            left = 0
-            top = 0
-            right = width - 0
-            bottom = height - 60
+            '''
+            if your crop parameters are not static, you can set it like this:
+                left = 0
+                top = 0
+                right = width - 0
+                bottom = height - 60
+            '''
+
+            # catch empty params
+            if args.left is None:
+                left = 0
+            else:
+                left = int(args.left)
+
+            if args.top is None:
+                top = 0
+            else:
+                top = int(args.top)
+
+            if args.right is None:
+                right = width - 0
+            else:
+                right = int(args.right)
+
+            if args.bottom is None:
+                bottom = height - 0
+            else:
+                bottom = int(args.bottom)
 
             # crop image
-            im_crpd = im.crop((left, top, right,  bottom))
+            image_crpd = image.crop((left, top, right, bottom))
 
-            # TODO join pathes with os.path.join()
-            im_crpd.save(f'{cwd}{dirname_to_crop}{name}_crpd{ext}')
+            # save image with new name
+            if args.destination:
+                image_crpd.save(os.path.join(args.destination,
+                                             f'{name}_crpd{ext}'))
+            else:
+                image_crpd.save(os.path.join(args.source,
+                                             f'{name}_crpd{ext}'))
+
             counter += 1
+
     except UnidentifiedImageError:
         pass
 
-print(f'{counter} images was cropped')
+
+# print results
+print(f'Amount of cropped images: {counter}')
